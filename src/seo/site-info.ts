86bc2d6f50854f4c7cffe660573483a6c6b044e4
Camilo Romero <camids980@gmail.com>
2024-08-26 18:48:39 -0400

    Pagina de pokemones hecha con la PokeApi, referencia de avanze en seccion 4 cap 50 de Astro

---
+++ src/seo/site-info.ts
@@ -0,0 +1,6 @@
+export const siteInfo = {
+  title: "Pokédex",
+  description: "Pagina web de la pokédex usando la PokeApi",
+  image:
+    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png",
+};
